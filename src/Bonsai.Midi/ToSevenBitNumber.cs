fba19d124be48372adaf4ee75e0dff95666b5f9f
glopesdev <glopesdev@users.noreply.github.com>
2024-10-06 14:46:17 +0100

    Add operators for converting MIDI values

---
+++ src/Bonsai.Midi/ToSevenBitNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Melanchall.DryWetMidi.Common;
+
+namespace Bonsai.Midi
+{
+    /// <summary>
+    /// Represents an operator that converts a sequence of 8-bit unsigned integer values
+    /// to a sequence of 7-bit numbers.
+    /// </summary>
+    [Description("Converts a sequence of 8-bit unsigned integer values to a sequence of 7-bit numbers.")]
+    public class ToSevenBitNumber : Transform<byte, SevenBitNumber>
+    {
+        /// <summary>
+        /// Converts all 8-bit unsigned integer values in an observable sequence
+        /// to 7-bit numbers.
+        /// </summary>
+        /// <param name="source">A sequence of 8-bit unsigned integer values.</param>
+        /// <returns>A sequence of 7-bit numbers.</returns>
+        public override IObservable<SevenBitNumber> Process(IObservable<byte> source)
+        {
+            return source.Select(value => (SevenBitNumber)value);
+        }
+    }
+}
