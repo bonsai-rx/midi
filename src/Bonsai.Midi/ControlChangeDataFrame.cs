3caea139f977a5dae12593db91f288ef1882fedb
ncguilbeault <ncguilbeault@gmail.com>
2026-08-11 09:38:31 +0100

    Refactor ControlChange to emit a dedicated ControlChangeDataFrame struct
    ControlChange now projects each ControlChangeEvent into a ControlChangeDataFrame value exposing the channel, control number, and control value as plain integers, so downstream operators can consume control change data without depending on DryWetMidi types.

---
+++ src/Bonsai.Midi/ControlChangeDataFrame.cs
@@ -0,0 +1,32 @@
+﻿namespace Bonsai.Midi
+{
+    /// <summary>
+    /// Represents the channel, control number, and control value of a MIDI
+    /// control change event.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="ControlChangeDataFrame"/> structure
+    /// with the specified channel, control number, and control value.
+    /// </remarks>
+    /// <param name="channel">The MIDI channel on which the control change event was raised.</param>
+    /// <param name="controlNumber">The number of the MIDI controller that changed value.</param>
+    /// <param name="controlValue">The new value of the MIDI controller.</param>
+    public readonly struct ControlChangeDataFrame(int channel, int controlNumber, int controlValue)
+    {
+
+        /// <summary>
+        /// Gets the MIDI channel on which the control change event was raised.
+        /// </summary>
+        public int Channel { get; } = channel;
+
+        /// <summary>
+        /// Gets the number of the MIDI controller that changed value.
+        /// </summary>
+        public int ControlNumber { get; } = controlNumber;
+
+        /// <summary>
+        /// Gets the new value of the MIDI controller.
+        /// </summary>
+        public int ControlValue { get; } = controlValue;
+    }
+}
